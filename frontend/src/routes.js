ac5130d265a3f8a476ed8c9309647cc47028a163
annemoraes <ilarymoraes@hotmail.com>
2020-10-19 16:36:19 -0300

    pagina para registrar novos clientes do banco

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Choice from './pages/Choice';
 import Credit from './pages/Credit';
 import Debit from './pages/Debit';
+import Register from './pages/Register';
 
 
 export default function Routes(){
@@ -13,6 +14,7 @@ export default function Routes(){
         <Route path="/" exact component={Choice}/>
         <Route path="/credit" component={Credit}/>
         <Route path="/debit" component={Debit}/>
+        <Route path="/register" component={Register}/>
       </Switch>
     </BrowserRouter>
   );
